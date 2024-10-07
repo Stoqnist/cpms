35349154ee613d79a753a0038af54d0044c88ce9
Stoyan Georgiev <stoyan.georgiev.ext@docebo.com>
2024-10-07 10:58:33 +0300

    chore: add dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
@@ -6,18 +7,24 @@ export class MyStack extends Stack {
     super(scope, id, props);
 
     // define resources here...
+
   }
 }
 
 // for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
+// const devEnv = {
+//   account: process.env.DEV_ACCOUNT,
+//   region: process.env.DEV_REGION,
+// };
+
+const prodEnv = {
+  account: process.env.PROD_ACCOUNT,
+  region: process.env.PROD_REGION,
 };
 
 const app = new App();
 
-new MyStack(app, 'cpms-dev', { env: devEnv });
-// new MyStack(app, 'cpms-prod', { env: prodEnv });
+// new MyStack(app, 'cpms-dev', { env: devEnv });
+new MyStack(app, 'cpms-prod', { env: prodEnv });
 
 app.synth();
